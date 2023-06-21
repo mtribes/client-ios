605c393e6082884e02e76e4b6ff5df712c491d5b
Chris Palmer <77305662+chris-palmer-deltatre@users.noreply.github.com>
2023-06-21 14:00:15 +0900

    fix: add spm support (#15)

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "Mtribes",
+    platforms: [
+        .iOS(.v10),
+        .tvOS(.v10),
+        .macOS(.v10_12),
+        .watchOS(.v3),
+    ],
+    products: [
+        .library(
+            name: "Mtribes",
+            targets: ["Mtribes"]),
+    ],
+    targets: [
+        .binaryTarget(name: "Mtribes", path: "Mtribes.xcframework")
+    ]
+)
